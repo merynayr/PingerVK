c279e3f865cd5f93eb7b15e6ee590238a79107d8
merynayr <boyarkin_dima2@mail.ru>
2025-02-05 11:49:02 +0300

    Create auxiliary packages

--- pkg/sys/error.go
+++ pkg/sys/error.go
@@ -6,11 +6,13 @@ import (
 	"github.com/merynayr/PingerVK/pkg/sys/codes"
 )
 
+// Структура для обработчки http ошибок
 type commonError struct {
 	msg  string
 	code codes.Code
 }
 
+// NewCommonError создаёт новвую ошибку
 func NewCommonError(msg string, code codes.Code) *commonError {
 	return &commonError{msg, code}
 }
@@ -23,11 +25,13 @@ func (r *commonError) Code() codes.Code {
 	return r.code
 }
 
+// IsCommonError проверяет на соответствие ошибке
 func IsCommonError(err error) bool {
 	var ce *commonError
 	return errors.As(err, &ce)
 }
 
+// GetCommonError получает ошбику
 func GetCommonError(err error) *commonError {
 	var ce *commonError
 	if !errors.As(err, &ce) {
